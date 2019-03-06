86b3ba4170fd5577ecf7e007d3b1664ad4942ae0
DavidPalma <davidpalmalugo@gmail.com>
2019-03-06 05:33:11 -0430

    Se agregaron funcionalidades en index y app js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const app = new Vue({
   el: '#app',
   data: {
-    titulo:'Gym con Vue'
+    titulo:'Gym con Vue',
+    tareas: [],
+    nuevaTarea: ''
+  },
+  methods:{
+    agregarTarea: function(){
+      this.tareas.push({ //push sirve para agregar items dentro de un array
+        nombre: this.nuevaTarea,
+        estado: false
+      });
+      console.log(this.tareas)
+      this.nuevaTarea = "";
+    }
   }
 });
